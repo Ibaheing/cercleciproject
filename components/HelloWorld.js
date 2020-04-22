52d9a4e03d97cf334918b1066e777d788a1d87a5
Heiding Ibaheing <H311189A@reziecomm.net>
2020-04-22 06:22:15 +0000

    first commit

--- components/HelloWorld.js
+++ components/HelloWorld.js
@@ -3,7 +3,7 @@ const HelloWorld = () => {
   return (
     <div className='hello-world'>
       <h1>Simple React App</h1>
-      <p>For CircleCI CICD setup today 4-22-2020</p>
+      <p>For CircleCI CICD setup test</p>
     </div>
   )
 }
